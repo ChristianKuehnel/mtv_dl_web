576ae3472cb39ab5cf64d8ea4e549bec50eb6880
Christian Kühnel <christian.kuehnel@gmail.com>
2022-01-03 16:04:48 +0000

    serving results live from mtv_dl

--- mtv_dl_web.py
+++ mtv_dl_web.py
@@ -2,18 +2,27 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import subprocess
 
-hostName = "localhost"
-serverPort = 8080
+
+class Config:
+    # TODO: load config from file
+    def __init__(self):
+        self.hostName = "localhost"
+        self.serverPort = 8080
+        self.mtv_dl_binary = "mtv_dl.py"
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        # TODO: add "/favicon.ico"
         print(self.path)
         if self.path == "/":
             self.send_file("index.html")
-        elif self.path == "/search.js":
-            self.send_file("search.js")
+        # elif self.path == "/search.js":
+        #     self.send_file("search.js")
+        elif self.path == "/search":
+            self.mtv_dl_dump()
 
     def send_file(self, filename):
         self.send_response(200)
@@ -21,10 +30,21 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(open(filename, "rb").read())
 
+    def mtv_dl_dump(self):
+        cmd = 'mtv_dl -r 24 dump topic="Nord bei Nordwest"'
+        print(f"Starting mtv_dl: {cmd}")
+        json = subprocess.check_output(cmd, shell=True)
+        print("mtv_dl finished")
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+        self.wfile.write(json)
+
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    config = Config()
+    webServer = HTTPServer((config.hostName, config.serverPort), MyServer)
+    print("Server started http://%s:%s" % (config.hostName, config.serverPort))
 
     try:
         webServer.serve_forever()
