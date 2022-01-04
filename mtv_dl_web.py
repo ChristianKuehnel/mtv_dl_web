5fe603763d63c267aa476ee78fce68d43bffbdc9
Christian Kühnel <christian.kuehnel@gmail.com>
2022-01-04 15:40:33 +0000

    implemented download

--- mtv_dl_web.py
+++ mtv_dl_web.py
@@ -12,7 +12,8 @@ class Config:
     def __init__(self):
         self.hostName = "localhost"
         self.serverPort = 8080
-        self.mtv_dl_binary = "mtv_dl.py"
+        self.mtv_dl_binary = "mtv_dl"
+        self.mtv_dl_args = []
 
 
 class MyServer(BaseHTTPRequestHandler):
@@ -36,15 +37,16 @@ class MyServer(BaseHTTPRequestHandler):
 
     def mtv_dl_dump(self):
         # TODO: implement error handling
+        # TODO: use better handling of GET parameters
         m = re.search(r"/search\?query=(.*)", self.path)
 
         query = unquote(m.group(1))
 
         # TODO: This gives you remote code execution!
         #       sanitize inputs before running the command
-        cmd = f"mtv_dl -r 24 dump {query}"
-        print(f"Starting mtv_dl: {cmd}")
-        json = subprocess.check_output(cmd, shell=True)
+        cmd = ["mtv_dl", "-r 24", "dump", query]
+        print(f"Starting mtv_dl: {' '.join(cmd)}")
+        json = subprocess.check_output(cmd)
         print("mtv_dl finished")
         self.send_response(200)
         self.send_header("Content-type", "text/json")
@@ -52,17 +54,27 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(json)
 
     def mtv_dl_download(self):
-        # TODO: implement downloading
-        # TODO: implement queing
-        # TODO: what do we return here?
-        pass
+        # TODO: implement error handling
+        # TODO: implement queing, mtv_dl can only handle one download at a time
+        # TODO: use better handling of GET parameters
+        m = re.search(r"/download\?hash=(.*)", self.path)
+        hash = m.group(1)
+        cmd = ["mtv_dl", "-r 23", "download", f"hash={hash}"]
+        print(f"Downloading {hash}")
+        print(f"Starting mtv_dl: {' '.join(cmd)}")
+        subprocess.check_call(cmd)
+        print(f"Download {hash} completed.")
+        self.send_response(200)
+        self.send_header("Content-type", "text/text")
+        self.end_headers()
+        self.wfile.write("Download started.".encode("utf-8"))
 
 
 if __name__ == "__main__":
     config = Config()
     webServer = HTTPServer((config.hostName, config.serverPort), MyServer)
     print("Server started http://%s:%s" % (config.hostName, config.serverPort))
-
+    # TODO: auto-update database on startup and every 24 hours
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
